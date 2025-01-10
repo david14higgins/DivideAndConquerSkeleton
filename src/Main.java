54132f3bad00d089718de82537883ccda3c0a697
David Higgins <david14higgins@gmail.com>
2025-01-10 15:35:00 +0000

    read probe times into main process

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
+
+
     public static void main(String[] args) {
         StrassensMatrixMultiplicationImpl strassensMatrixMultiplication = new StrassensMatrixMultiplicationImpl();
         probeSkeletonImplementation(strassensMatrixMultiplication);
@@ -11,6 +14,10 @@ public class Main {
     }
 
     public static void probeSkeletonImplementation(Object skeletonImpl) {
+        HashMap<Integer, Long> solverRuntimes = new HashMap<>();
+        HashMap<Integer, Long> dividerRuntimes = new HashMap<>();
+        HashMap<Integer, Long> combinerRuntimes = new HashMap<>();
+
         try {
             // Build the command to start a new Java process
             String javaHome = System.getProperty("java.home");
@@ -33,14 +40,26 @@ public class Main {
                 String line;
                 while ((line = reader.readLine()) != null) {
                     System.out.println(line);
+
+                    String[] lineValues = line.split(" ");
+                    switch (lineValues[0]) {
+                        case "SOLVER": solverRuntimes.put(Integer.getInteger(lineValues[1]), Long.valueOf(lineValues[2])); break;
+                        case "DIVIDER": dividerRuntimes.put(Integer.getInteger(lineValues[1]), Long.valueOf(lineValues[2])); break;
+                        case "COMBINER": combinerRuntimes.put(Integer.getInteger(lineValues[1]), Long.valueOf(lineValues[2])); break;
+                        default: break;
+                    }
                 }
             }
-
             process.waitFor(); // Wait for the process to complete
 
             System.out.println("Still executing in Main.java");
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        //Output read values
+        System.out.println(solverRuntimes);
+        System.out.println(dividerRuntimes);
+        System.out.println(combinerRuntimes);
     }
 }
